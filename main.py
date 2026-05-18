ddf2ed4663f9a412869a91bc960ef95d2866f316
jirapat33 <jirapat.peef65@gmail.com>
2026-05-18 15:33:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.add_middleware(
 
 # 🔥 ใส่ API Key ของ ImgBB ที่คุณสมัครได้ที่นี่
 # แก้ไขบรรทัด IMGBB_API_KEY ใน main.py บน GitHub ให้เป็นแบบนี้:
-IMGBB_API_KEY = os.environ.get("IMGBB_API_KEY", "c299dc524f2bc43c2d766741c0a83047")
+IMGBB_API_KEY = "c299dc524f2bc43c2d766741c0a83047"
 
 class CheckInSchema(BaseModel):
     studentId: str
